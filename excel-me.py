f01879a6a2e6795290be1678ca6959d873c290e4
Abir Hasan <abir.spsc@gmail.com>
2021-02-01 20:49:36 +0600

    Add functions image_to_list, resize_image

--- excel-me.py
+++ excel-me.py
@@ -8,14 +8,34 @@ from openpyxl.styles.differential import DifferentialStyle
 
 
 def main(arg):
-    image = read_image(arg)
-    img_array = np.array(image)
-    image.show()
 
-    filename = get_filename(arg) + ".xlsx"
+    image = read_image(arg)
+    # Make the image smaller
+    image = resize_image(image, 240, 352)   # height, width for 240p
+    list = image_to_list(image)
+    #image.show()
+
+    filename = get_filename(arg)
     wb = Workbook()
+    sheet = wb.active
+
+    for row in list:
+        sheet.append(row)
 
     wb.save(filename=filename)
+
+
+def image_to_list(image):
+    """Convert image to tabular list."""
+    arr = np.array(image)
+    h, w, p = arr.shape[0], arr.shape[1], arr.shape[2]
+    arr = arr.reshape((h, w*p))
+    return arr.tolist()
+
+
+def resize_image(image, height, width):
+    """Resize the image (to work with less values)."""
+    return image.resize((height, width))
 
 
 def read_image(path):
@@ -28,9 +48,12 @@ def read_image(path):
 
 
 def get_filename(path):
-    """Parse filepath and retuen the name part."""
+    """Parse filepath, remove old file and retuen the filename."""
     name = os.path.basename(path).lower()
-    name = name.replace(".jpeg", "").replace(".jpg", "").replace(".png", "").replace(".png", "")
+    name = name.replace(".jpeg", "").replace(".jpg", "").replace(".png", "")
+    name += ".xlsx"
+    if os.path.exists(name):
+        os.remove(name)
     return name
 
 
