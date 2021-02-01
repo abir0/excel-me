55538e6f1ec2e1011d5a03c05dd03fbe9efc0f80
Abir Hasan <abir.spsc@gmail.com>
2021-02-01 17:32:39 +0600

    Add function get_filename and open workbook

--- excel-me.py
+++ excel-me.py
@@ -1,10 +1,22 @@
-import sys
+import sys, os
 from PIL import Image
 import numpy as np
+from openpyxl import Workbook
+from openpyxl.formatting import Rule
+from openpyxl.styles import Font, PatternFill, Border
+from openpyxl.styles.differential import DifferentialStyle
+
 
 def main(arg):
     image = read_image(arg)
+    img_array = np.array(image)
     image.show()
+
+    filename = get_filename(arg) + ".xlsx"
+    wb = Workbook()
+
+    wb.save(filename=filename)
+
 
 def read_image(path):
     """Read an image from a file path"""
@@ -13,6 +25,13 @@ def read_image(path):
         return image
     except Exception as e:
         print(e)
+
+
+def get_filename(path):
+    """Parse filepath and retuen the name part."""
+    name = os.path.basename(path).lower()
+    name = name.replace(".jpeg", "").replace(".jpg", "").replace(".png", "").replace(".png", "")
+    return name
 
 
 if __name__ == "__main__":
