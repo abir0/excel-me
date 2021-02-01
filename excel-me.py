7a3dbb42f379040709617e2f1f2d09316cb400f3
Abir Hasan <abir.spsc@gmail.com>
2021-02-01 22:28:12 +0600

    Add conditional formatting

--- excel-me.py
+++ excel-me.py
@@ -2,9 +2,9 @@ import sys, os
 from PIL import Image
 import numpy as np
 from openpyxl import Workbook
-from openpyxl.formatting import Rule
-from openpyxl.styles import Font, PatternFill, Border
-from openpyxl.styles.differential import DifferentialStyle
+from openpyxl.formatting.rule import ColorScaleRule
+from openpyxl.styles import colors
+from openpyxl.utils import get_column_letter
 
 
 def main(arg):
@@ -12,15 +12,48 @@ def main(arg):
     image = read_image(arg)
     # Make the image smaller
     image = resize_image(image, 240, 352)   # height, width for 240p
-    list = image_to_list(image)
+    list, width, height = image_to_list(image)
     #image.show()
 
     filename = get_filename(arg)
     wb = Workbook()
     sheet = wb.active
 
+    # Apend the pixel values to worksheet
     for row in list:
         sheet.append(row)
+
+    # Make conditional formatting rules
+    color_scale_rule_red = ColorScaleRule(start_type="num",
+                                      start_value=0,
+                                      start_color="000000",
+                                      end_type="num",
+                                      end_value=255,
+                                      end_color="FF0000")
+    color_scale_rule_green = ColorScaleRule(start_type="num",
+                                      start_value=0,
+                                      start_color="000000",
+                                      end_type="num",
+                                      end_value=255,
+                                      end_color="00FF00")
+    color_scale_rule_blue = ColorScaleRule(start_type="num",
+                                      start_value=0,
+                                      start_color="000000",
+                                      end_type="num",
+                                      end_value=255,
+                                      end_color="0000FF")
+
+    # Add the rules to worksheet
+    for i in range(1, width + 1):
+        col = get_column_letter(i)
+        string = col + "1" + ":" + col + str(height)
+        #print("Adding color rule to cells", string)
+        if i % 3 == 1:
+            sheet.conditional_formatting.add(string, color_scale_rule_red)
+        elif i % 3 == 2:
+            sheet.conditional_formatting.add(string, color_scale_rule_green)
+        elif i % 3 == 0:
+            sheet.conditional_formatting.add(string, color_scale_rule_blue)
 
     wb.save(filename=filename)
 
@@ -28,9 +61,9 @@ def main(arg):
 def image_to_list(image):
     """Convert image to tabular list."""
     arr = np.array(image)
-    h, w, p = arr.shape[0], arr.shape[1], arr.shape[2]
+    w, h, p = arr.shape[0], arr.shape[1], arr.shape[2]
     arr = arr.reshape((h, w*p))
-    return arr.tolist()
+    return arr.tolist(), (w*p), h
 
 
 def resize_image(image, height, width):
